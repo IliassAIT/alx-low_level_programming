c0f53b9a8c159d009909a88bd16eea7bf795d935
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2023-07-09 19:38:41 +0000

    adding the attribute unused to the argc parameter

--- 0x0A-arc_argv/0-whatsmyname.c
+++ 0x0A-arc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: Arguments vector which is an array of pointers
  * Return: return (0) success
  */
-int main(char argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char* argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
