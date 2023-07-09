bc84c9088ad49af73cacc6efb3b594d6d2786348
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2023-07-09 20:17:31 +0000

    the error was because i declare and attributed values to the operands before the if check

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-int a = atoi(argv[1]), b = atoi(argv[2]), c;
+int a, b, c;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 c = a * b;
 printf("%d\n", c);
 return (0);
