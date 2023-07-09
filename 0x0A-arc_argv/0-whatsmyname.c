c6b07b1085aa5d60da7a117fb120de1cb538042f
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2023-07-09 19:26:25 +0000

    first task

---
+++ 0x0A-arc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: return (0) success
+ */
+int main(char argc, char* argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-arc_argv/README.md
@@ -0,0 +1,1 @@
+Month #1 - Task 0x0A : Argument Count (argc) && Argument Vector (argv)
