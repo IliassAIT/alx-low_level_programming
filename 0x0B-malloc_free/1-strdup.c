8cece0ac40aeb11d2496e471144fbace8e5ed2eb
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2023-07-10 20:26:11 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strdup - func that duplicates a string
+ * @str: str to be duplicated
+ * Return: return (ptr) to the new string or NULL
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int i, j;
+if (!str)
+	return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+dup = malloc(sizeof(char) * i);
+if (!dup)
+	return (NULL);
+for (j = 0; j < i; j++)
+{
+dup[j] = str[j];
+}
+return (dup);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,5 @@
 #include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int sie, char c);
+char *_strdup(char *str);
 #endif
