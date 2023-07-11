5bbf1f1b93cf171f72588412106819d440031910
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2023-07-11 20:02:40 +0000

    free func to free the grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - function that frees a grid
+ * @grid: 2d array to be freed.
+ * @height: the rows of the array.
+ */
+void free_grid(int **grid, int height)
+{
+int h;
+if (grid != NULL)
+{
+for (h = 0; h <= height; h++)
+{
+	free(grid[h]);
+}
+free(grid);
+}
+}
