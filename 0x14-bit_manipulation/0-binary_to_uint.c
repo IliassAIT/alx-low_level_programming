2476d4646f2f3b05565531608a1ae6550766c8a4
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2023-08-04 01:56:53 +0000

    task 0 fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,11 @@ unsigned int ind = 1;
 unsigned int i = 0;
 if (!b)
 	return (0);
-while (b[i] != 0)
+while (b[i] != '\0')
 {
-if (b[i] < '0' || b[i] > '9';
+if (b[i] != '0' && b[i] != '1')
 	return (0);
-dec += (b[i] - '0') * ind;
+dec = dec * 2 + (b[i] - 48);
 ind *= 2;
 i++;
 }
