2e4e1daeaeb7446e4f96be97621c1e4f10d77713
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2024-01-12 23:11:26 +0000

    quick fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,6 +11,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 int cent_types[] = {1, 2, 5, 10, 25};
 int cash = atoi(argv[1]);
 int i = 4, count = 0;
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
+}
+if (cash < 0)
+	printf("0\n");
+else
+{
 while (i > 0)
 {
 if (cash < cent_types[i])
@@ -22,5 +31,6 @@ i--;
 if (cash == 1)
 	count++;
 printf("%d\n", count);
+}
 return (0);
 }
