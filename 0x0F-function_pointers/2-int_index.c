b34edee58111d10ade43d49e9872f09329805966
Iliass AIT HAMOU <aithamouiliass@gmail.com>
2024-01-25 00:46:59 +0000

    functions pointers !

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that looks for an int
+ * @array: the input data array
+ * @size: the length of the array
+ * @cmp: the pointer to the function
+ * Return: index to element if found or -1 if failed
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0 || !array || !cmp)
+	return (-1);
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == 1)
+	return (i);
+}
+}
+return (-1);
+}
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+/**
+ * get_op_func - function selecter
+ * @s: a pointer to the string of the selected op
+ * Return: pointer to the selected op or (NULL)
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+int flag;
+flag = 0;
+while (flag < 5)
+{
+	if (strcmp(s, ops[flag].op) == 0)
+		return (ops[flag].f);
+	flag++;
+	}
+return (NULL);
+}
