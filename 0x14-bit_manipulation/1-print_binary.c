86070b3d5f305b057dfcf16b069d7ed9a597301e
IliassAt <aithamouiliass@gmail.com>
2024-02-09 00:34:48 +0000

    task two, i mean two - 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - function that converts a decimal to binary and prints it
+ * @n: decimal to be printed.s
+ */
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+	_putchar('0');
+	return;
+}
+if (n > 0)
+	print_binary(n >> 1);
+if ((n | 1) == n)
+	_putchar('1');
+if ((n | 1) != n)
+	_putchar('0');
+}
